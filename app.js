6caf6a2bf6f19cdeb75b78cd138feade13949dde
Hunter <88465412+Hunyeh@users.noreply.github.com>
2021-10-12 17:17:31 -0400

    adfgadfg

--- app.js
+++ app.js
@@ -10,17 +10,23 @@
 // printProfileData(profileDataArgs);
 
 
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
+const inquirer = require('inquirer')
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name1, github] = profileDataArgs;
-console.log(name1, github);
+// const pageHTML = generatePage(name1, github);
 
-const pageHTML = generatePage(name1, github);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers))
